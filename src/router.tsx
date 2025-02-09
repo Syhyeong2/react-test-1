75411e571d6650a7dd4575ac2fdebc057dd73765
sihyeong <syhy7051@gmail.com>
2025-02-09 17:17:34 +0900

    asd

--- src/router.tsx
+++ src/router.tsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/index.html",
         element: <Homepage />,
       },
       {
