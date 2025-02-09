755b3f6348d55a666e82b5f782c802a4961a016f
sihyeong <syhy7051@gmail.com>
2025-02-09 17:06:59 +0900

    Asd

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "./build",
+    assetsDir: "./",
+  },
+  base: "./",
+  server: {
+    port: 3000,
+  },
 });
